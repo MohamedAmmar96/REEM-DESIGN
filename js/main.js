10a9de9b60122dd2d033b47ca1cbb64f58a09541
Mohamed Ammar <ammarmoahmed074@gmail.com>
2020-08-11 03:58:51 +0200

    Adding WOW.Js Animation with some Media Edits

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(window).on('load', function() {
         function() {
             $(this).parent().fadeOut(1000);
             $("body").removeClass("overflow")
+            $("body").css("overflow-X", "hidden");
         });
 })
 
@@ -18,6 +19,20 @@ scrollButton.click(function() {
     $("html,body").animate({ scrollTop: 0 }, 600);
 });
 
+var wow = new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+});
+wow.init();
 
 $(document).ready(function() {
 
@@ -284,6 +299,8 @@ $(document).ready(function() {
     $(".black-back").click(function() {
         $(".black-back").fadeOut(600)
     });
+
+
 
     //this is to show/hide map
     $(".my-form p").click(function() {
